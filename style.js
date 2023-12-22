0080e1346436ee5c2c40d7c15a65274acb0831ca
Oleg1224 <olegik.kr@gmail.com>
2023-12-22 23:40:28 +0200

    Initial commit

---
+++ style.js
@@ -0,0 +1,119 @@
+let head = document.querySelector('.head__bgraund');
+window.addEventListener('scroll', function() {
+    if (this.scrollY > 10 ) {
+        head.classList.add('new')
+    }
+    else {
+        head.classList.remove('new')
+    }
+})
+let cardDesigns = document.querySelector('.card-designs');
+let modal1 = document.querySelector('.modal1');
+cardDesigns.addEventListener('click', function() {
+    if (modal1.className === 'modal1') {
+        modal1.classList.add('designs')
+    }
+    else {
+        modal1.classList.remove('designs')
+    }
+})
+let designs1 = document.querySelector('.designs1');
+let card = document.querySelector('.main__block2-card');
+designs1.addEventListener('click', function() {
+    if (card.className === 'main__block2-card') {
+        card.classList.add('designs1')
+    }
+    else {
+        card.classList.remove('designs1')
+    }
+})
+let designs2 = document.querySelector('.designs2');
+// let card = document.querySelector('.main__block2-card');
+designs2.addEventListener('click', function() {
+    if (card.className === 'main__block2-card') {
+        card.classList.add('designs2')
+    }
+    else {
+        card.classList.remove('designs2')
+    }
+})
+let designs3 = document.querySelector('.designs3');
+// let card = document.querySelector('.main__block2-card');
+designs3.addEventListener('click', function() {
+    if (card.className === 'main__block2-card') {
+        card.classList.add('designs3')
+    }
+    else {
+        card.classList.remove('designs3')
+    }
+})
+let designs4 = document.querySelector('.designs4');
+// let card = document.querySelector('.main__block2-card');
+designs4.addEventListener('click', function() {
+    if (card.className === 'main__block2-card') {
+        card.classList.add('designs4')
+    }
+    else {
+        card.classList.remove('designs4')
+    }
+})
+let designs5 = document.querySelector('.designs5');
+// let card = document.querySelector('.main__block2-card');
+designs5.addEventListener('click', function() {
+    if (card.className === 'main__block2-card') {
+        card.classList.add('designs5')
+    }
+    else {
+        card.classList.remove('designs5')
+    }
+})
+let designs6 = document.querySelector('.designs6');
+// let card = document.querySelector('.main__block2-card');
+designs6.addEventListener('click', function() {
+    if (card.className === 'main__block2-card') {
+        card.classList.add('designs6')
+    }
+    else {
+        card.classList.remove('designs6')
+    }
+})
+let designs7 = document.querySelector('.designs7');
+// let card = document.querySelector('.main__block2-card');
+designs7.addEventListener('click', function() {
+    if (card.className === 'main__block2-card') {
+        card.classList.add('designs7')
+    }
+    else {
+        card.classList.remove('designs7')
+    }
+})
+let designs8 = document.querySelector('.designs8');
+// let card = document.querySelector('.main__block2-card');
+designs8.addEventListener('click', function() {
+    if (card.className === 'main__block2-card') {
+        card.classList.add('designs8')
+    }
+    else {
+        card.classList.remove('designs8')
+    }
+})
+let designs9 = document.querySelector('.designs9');
+// let card = document.querySelector('.main__block2-card');
+designs9.addEventListener('click', function() {
+    if (card.className === 'main__block2-card') {
+        card.classList.add('designs9')
+    }
+    else {
+        card.classList.remove('designs9')
+    }
+})
+let designs10 = document.querySelector('.designs10');
+// let card = document.querySelector('.main__block2-card');
+designs10.addEventListener('click', function() {
+    if (card.className === 'main__block2-card') {
+        card.classList.add('designs10')
+    }
+    else {
+        card.classList.remove('designs10')
+    }
+})

6609f853b47f1e3c7e0c16e969ed57917cd4fa88
Oleg1224 <olegik.kr@gmail.com>
2023-12-26 07:50:00 +0200

    1

--- Card/card.js
+++ Card/card.js
@@ -1,8 +1,35 @@
-let block2 = document.querySelector('.block2');
-let cardInside = document.querySelector('.block2-cardinside');
+// let block2 = document.querySelector('.block2');
+// let cardInside = document.querySelector('.block2-cardinside');
+// let input = document.querySelector('.input__text');
+// let btn = document.querySelector('.block2__card-text2');
+// let choose = document.querySelector('.btn__text')
+// choose.addEventListener('click', function() {
+//     btn.textContent = input.value
+// })
+
 let input = document.querySelector('.input__text');
 let btn = document.querySelector('.block2__card-text2');
-let choose = document.querySelector('.btn__text')
-choose.addEventListener('click', function() {
-    input.value = btn.textContent
+input.addEventListener('input', function() {
+    btn.textContent = input.value
 })
+
+let inputDate = document.querySelector('.input__date');
+let btn2 = document.querySelector('.block2__card-text1')
+inputDate.addEventListener('input', function() {
+    btn2.textContent = inputDate.value
+})
+
+let firstCard = document.querySelector('.first-card');
+let block2Text = document.querySelector('.card-text');
+    // window.location.href='../Card/card.html'
+    let generationCard = Math.floor(Math.random() * 9000 + 1000)
+    let generationCardFirst = Math.floor(Math.random() * 9000 + 1000)
+    let generationCardSecond = Math.floor(Math.random() * 9000 + 1000)
+    block2Text.textContent = `2456 ${generationCard} ${generationCardFirst} ${generationCardSecond}` 
+// let secondCard = document.querySelector('.second-card');
+// let card2 = document.querySelector('.card2')
+// secondCard.addEventListener('click', function() {
+//     window.location.href='../Card/card.html'
+//     /* card.backgroundImage = url('../img/Человек паук 2 обом.jpg') */
+//     /* card2.style.backgroundImage.url = '../img/Человек\ паук\ 2\ обом.jpg' */
+// })
---
+++ Created card/created-card.js
@@ -0,0 +1,48 @@
+let firstCard = document.querySelector('.first-card');
+let block2Text = document.querySelector('.block2__card-text');
+firstCard.addEventListener('click', function() {
+    window.location.href='../Card/card.html'
+})
+
+let secondCard = document.querySelector('.second-card');
+secondCard.addEventListener('click', function() {
+    window.location.href='../Card/card.html'
+})
+let thirdCard = document.querySelector('.third-card');
+thirdCard.addEventListener('click', function() {
+    window.location.href='../Card/card.html'
+})
+let fourthCard = document.querySelector('.fourth-card');
+fourthCard.addEventListener('click', function() {
+    window.location.href='../Card/card.html'
+})
+let fifthCard = document.querySelector('.fifth-card');
+fifthCard.addEventListener('click', function() {
+    window.location.href='../Card/card.html'
+})
+let sixthCard = document.querySelector('.sixth-card');
+sixthCard.addEventListener('click', function() {
+    window.location.href='../Card/card.html'
+})
+let seventhCard = document.querySelector('.seventh-card');
+seventhCard.addEventListener('click', function() {
+    window.location.href='../Card/card.html'
+})
+let eighthCard = document.querySelector('.eighth-card');
+eighthCard.addEventListener('click', function() {
+    window.location.href='../Card/card.html'
+})
+let ninthCard = document.querySelector('.ninth-card');
+ninthCard.addEventListener('click', function() {
+    window.location.href='../Card/card.html'
+})
+let tenthCard = document.querySelector('.tenth-card');
+tenthCard.addEventListener('click', function() {
+    window.location.href='../Card/card.html'
+})
+let eleventhCard = document.querySelector('.eleventh-card');
+eleventhCard.addEventListener('click', function() {
+    window.location.href='../Card/card.html'
+})
+/* card.backgroundImage = url('../img/Человек паук 2 обом.jpg') */
+    /* card2.style.backgroundImage.url = '../img/Человек\ паук\ 2\ обом.jpg' */

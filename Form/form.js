75049600dec53464ef4b888cb5ec6412098e43b6
Oleg1224 <olegik.kr@gmail.com>
2023-12-27 21:58:06 +0200

    1

--- Form/form.js
+++ Form/form.js
@@ -4,8 +4,17 @@ formBtn.addEventListener('click', function() {
     let passwordInput = document.querySelector('.password-input')
     let emailInput = document.querySelector('.email-input')
     let phoneInput = document.querySelector('.phone-input')
+    let checkInput = document.querySelector('.check-input')
+    let modalName = document.querySelector('.modal-name')
     if (nameInput.value === '') {
-        alert('Введите имя!!!!!')
+        // alert('Введите имя!!!!!')
+        modalName.classList.add('name')
+        setTimeout(function() {
+            modalName.classList.remove('name')
+            formLable.style.color = 'black'
+        }, 1500)
+        let formLable = document.querySelector('.form-lable')
+        formLable.style.color = 'red'
     }
     else if (passwordInput.value.length < 6) {
         alert('Слишком короткий пароль!!!!')
@@ -15,5 +24,11 @@ formBtn.addEventListener('click', function() {
     }
     else if (!phoneInput.value.includes('+380') || phoneInput.value.length !== 13) {
         alert('введите номер')
+    }
+    else if (!checkInput.checked) {
+        alert('Вы не agree')
+    }
+    else if (true) {
+        window.location.href='../Created card/created-card.html'
     }
 })
